f4acbe94c607b80d83a5c0c5e7b60d9e5165a670
BadRAM <dgamebillda@gmail.com>
2023-07-14 14:01:30 -0700

    Alpha 1 release version

--- unityproj/Assets/Scripts/Menu/doortest.cs
+++ unityproj/Assets/Scripts/Menu/doortest.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
-using UnityEngine.InputSystem.Android;
 
 public class doortest : MonoBehaviour
 {
